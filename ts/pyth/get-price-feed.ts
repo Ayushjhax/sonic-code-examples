d54a75c13cc3a5da830ad82ac103da821304e081
Saviour1001 <harshghodkar@gmail.com>
2025-03-02 17:00:16 +0530

    nit

--- ts/pyth/get-price-feed.ts
+++ ts/pyth/get-price-feed.ts
@@ -1,22 +1,19 @@
-import {HermesClient} from "@pythnetwork/hermes-client";
-
+import { HermesClient } from "@pythnetwork/hermes-client";
 
 (async () => {
-    const priceServiceConnection = new HermesClient(
-        "https://hermes.pyth.network/",
-        {}
-    );
+  const priceServiceConnection = new HermesClient(
+    "https://hermes.pyth.network/",
+    {}
+  );
 
-    // Price Feed Ids
-    // https://www.pyth.network/developers/price-feed-ids#stable
+  // Price Feed Ids
+  // https://www.pyth.network/developers/price-feed-ids#stable
 
-    const priceUpdateData = (
-        await priceServiceConnection.getLatestPriceUpdates(
-            ["0xb2748e718cf3a75b0ca099cb467aea6aa8f7d960b381b3970769b5a2d6be26dc"],
-            {encoding: "base64"}
-        )
-    );
+  const priceUpdateData = await priceServiceConnection.getLatestPriceUpdates(
+    // Price feed of Sonic/USD
+    ["0xb2748e718cf3a75b0ca099cb467aea6aa8f7d960b381b3970769b5a2d6be26dc"],
+    { encoding: "base64" }
+  );
 
-    console.log(priceUpdateData.parsed[0].price);
-
+  console.log(priceUpdateData.parsed[0].price);
 })();
